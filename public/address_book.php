a253aec2f4858b03d28f814dd39ab38b2ffb933b
Brett Cohen <brettgmf@Bretts-MacBook-Air.local>
2014-06-04 10:45:04 -0500

    Moved my class to a different file and include it. Also refactored code

--- public/address_book.php
+++ public/address_book.php
@@ -1,67 +1,6 @@
 <?php
 
-// $filename = 'address_book.csv';
-
-// function savefile($filename, $array) {
-//     // $filename = $savefilepath;
-//     $handle = fopen($filename, 'w');
-//     foreach ($array as $row) {
-//         fputcsv($handle, $row);
-//     }
-//     fclose($handle); 
-// }
-
-// function readcsv($filename) {
-//     $contents = [];
-//     if (is_readable($filename) && filesize($filename) > 0){
-//         $handle = fopen($filename, 'r');
-//         while(!feof($handle)) {
-//             $row = fgetcsv($handle);
-//             if (is_array($row)) {
-//             $contents[] = $row;
-//             }
-//         }
-//         fclose($handle);
-//     }
-//     return $contents;
-// } 
-
 include('classes/address_data_store.php');
-
-// class AddressDataStore {
-
-//     // public $filename = 'address_book.csv';
-//     public $filename = '';
-
-//     function __construct($nameOfFile)
-//     {
-//         $this->filename = $nameOfFile;
-//     }
-
-//     function readcsv(){
-//         $contents = [];
-//         if (is_readable($this->filename) && filesize($this->filename) > 0){
-//             $handle = fopen($this->filename, 'r');
-//             while(!feof($handle)) {
-//                 $row = fgetcsv($handle);
-//                 if (is_array($row)) {
-//                 $contents[] = $row;
-//                 }
-//             }
-//             fclose($handle);
-//         }
-//         return $contents;
-//     }
-
-//     function savefile($addresses_array) {
-//         $handle = fopen($this->filename, 'w');
-//         foreach ($addresses_array as $row) {
-//             fputcsv($handle, $row);
-//         }
-//         fclose($handle); 
-//     }
-
-// }
 
 $ads = new AddressDataStore('address_book.csv');
 // $ads->filename = 'address_book.csv';
